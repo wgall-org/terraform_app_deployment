d0a5804e7467f051e5ac381834b0f0f985abb624
wgall <wojtekgall2003@gmail.com>
2023-03-25 13:06:51 +0100

    Added locals template

---
+++ example_app/terraform/dev/frontend/container.tf
@@ -0,0 +1,41 @@
+resource "google_cloud_run_service" "container" {
+  name     = var.project
+  location = local.region
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/${local.projectid}/${local.image}:${local.tag}"
+        ports {
+          protocol       = "TCP"
+          container_port = 80
+        }
+      }
+    }
+
+
+  }
+  autogenerate_revision_name = true
+  metadata {
+    annotations = {
+      "run.googleapis.com/ingress" = "internal-and-cloud-load-balancing"
+    }
+
+  }
+}
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = google_cloud_run_service.container.location
+  project  = google_cloud_run_service.container.project
+  service  = google_cloud_run_service.container.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
