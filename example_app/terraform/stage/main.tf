3fd672ff4622bc0ff83922a0818f529677374ee7
wgall <wojtekgall2003@gmail.com>
2023-03-27 15:28:23 +0200

    Added stage

---
+++ example_app/terraform/stage/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.26.0"
+    }
+  }
+  backend "gcs" {
+
+      bucket = "storage"
+      prefix = "frontend/"
+  }
+}
+
