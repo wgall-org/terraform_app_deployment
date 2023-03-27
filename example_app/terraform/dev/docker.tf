bd5f1ccd8599e5ad980f733b525cb27494175b96
wgall <wojtekgall2003@gmail.com>
2023-03-27 16:51:38 +0200

    Fixed docker deployment

--- example_app/terraform/dev/docker.tf
+++ example_app/terraform/dev/docker.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "docker-compose" {
     provisioner "local-exec" {
 
-        command = "cd resources; ./composeup.sh"
+        command = "cd resources; ./composeup.sh ${local.project}"
 
     }
 }
